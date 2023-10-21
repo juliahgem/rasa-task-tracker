e9b6a32bb950cd98111bdbc3185648f19e33f588
Денис Заречнев <d.zarech@gmail.com>
2023-10-21 13:26:06 +0300

    Добавил сырой полуработающий Action на добавление задачи
    Его еще надо доделывать поэтому пока трогать не нужно

--- actions/actions.py
+++ actions/actions.py
@@ -3,8 +3,11 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
+import datetime
 from typing import Text, Dict, Any, List
 
+import psycopg2
+from psycopg2._psycopg import cursor
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -39,8 +42,30 @@ class ActionCreateTask(Action):
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
         task_name = tracker.get_slot("task_name")
-        dispatcher.utter_message(text=f"Задача {task_name} создана!")
+        task_name = task_name
+        try:
+            # пытаемся подключиться к базе данных
+            print("Я пытаюсь...")
+            conn = psycopg2.connect(dbname="postgres", host="localhost", user="postgres", password="postgres",
+                                    port="5433")
+            curs = conn.cursor()
+            try:
+                query = """INSERT INTO tasks (task_id, title, description, deadline_dt) VALUES (%s, %s, %s, %s);"""
+                task_id = 11
+                description = ""
+                deadline_dt = None
+                values = (task_id, task_name, description, deadline_dt)
+
+                curs.execute(query, values)
+                conn.commit()
+                dispatcher.utter_message(text=f"Задача \"{task_name}\" создана!")
+            except Exception as inst:
+                print(type(inst))
+                print(inst.args)
+
+        except:
+            # в случае сбоя подключения будет выведено сообщение в STDOUT
+            print('Can`t establish connection to database')
 
         return []
